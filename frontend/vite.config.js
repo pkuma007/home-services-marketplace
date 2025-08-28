ea6f4b5716efe87eec88819e67b4fe821b9f72a4
pkuma007 <pravin.jhablu@gmail.com>
2025-08-28 23:40:41 +0530

    config updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['rightbridge-x9o1.onrender.com'],
     host: true, // This makes the server accessible on the local network
     proxy: {
       '/api': {
